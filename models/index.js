610151f6aebf33e2933e561bff2a24afe3744fa2
Nsaafdllh <annisafadillah125@upi.edu>
2024-12-09 22:08:24 +0700

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const sequelize = require("../config/db");
+const User = require("./user");
+const Token = require("./token");
+User.hasMany(Token);
+Token.belongsTo(User);
+sequelize.sync({ alter: true });
+module.exports = { sequelize, User, Token };
---
+++ models/token.js
@@ -0,0 +1,7 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+const Token = sequelize.define("Token", {
+  token: { type: DataTypes.STRING, allowNull: false },
+  revoked: { type: DataTypes.BOOLEAN, defaultValue: false },
+});
+module.exports = Token;
